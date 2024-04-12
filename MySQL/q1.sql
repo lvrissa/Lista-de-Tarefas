d8afc79f6a1ade4499d61c3c82f642b25915739e
Jesse <lima9jesse@gmail.com>
2024-04-12 10:17:57 -0300

    alterando

--- MySQL/q1.sql
+++ MySQL/q1.sql
@@ -1,8 +1,15 @@
 /* 1. Como você criaria uma tabela em MySQL com os campos: id (auto incremento), nome (varchar 255), email (varchar 255) e idade (int)? */
 
-CREATE TABLE nomes (
+CREATE TABLE funcionarios (
     id int NOT NULL AUTO_INCREMENT,
     nome varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    idade int NOT NULL
-)
+    email varchar(255),
+    idade int,
+    PRIMARY KEY (id)
+);
+
+
+INSERT INTO funcionarios (nome,email,idade)
+VALUES ('Larissa','larissa@gmail.com', 18);
+
+SELECT * FROM funcionarios;
